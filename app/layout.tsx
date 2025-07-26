028cacc8061106b4274b6dc0e3df0eabe6485754
Krishnaprasath <230801093@rajalakshmi.edu.in>
2025-07-26 17:35:18 +0530

    updated icons and metadata in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,8 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'GDG REC',
+  description: 'Landing page for GDG REC',
 }
 
 export default function RootLayout({
@@ -17,6 +16,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/logo.png" />
+        <link rel="apple-touch-icon" href="/logo.png" />
         <style>{`
 html {
   font-family: ${GeistSans.style.fontFamily};
